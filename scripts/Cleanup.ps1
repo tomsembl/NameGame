f2471901ad2417c217f3612acdba3ea6bd87376b
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-25 23:32:26 +0800

    testing suite

---
+++ scripts/Cleanup.ps1
@@ -0,0 +1,5 @@
+﻿#cleanup orphaned chromes
+get-process | where-object -Property processname -Like 'chrome*' | Stop-Process -Force -ErrorAction SilentlyContinue
+
+#clean up localappdata temp
+Remove-Item -Force -Recurse -Path "C:\Users\antip\AppData\Local\Temp\scoped_dir*" -ErrorAction SilentlyContinue
--- scripts/runtest.ps1
+++ scripts/runtest.ps1
@@ -1,6 +1,14 @@
-﻿$num_players = 1
+﻿cd C:\github_code\NameGame\scripts
+$num_players = 1
 while($true){
-    write-host '`n`n$num_players'
-    python.exe web_scrape_test_headless.py 1
+    #Run test
+    write-host "`n`nnum_players: $num_players"
+    python.exe web_scrape_test.py 1 --headless
     $num_players ++
+
+    #cleanup orphaned chromes
+    get-process | where-object -Property processname -Like 'chrome*' | Stop-Process -Force -ErrorAction SilentlyContinue
+
+    #clean up localappdata temp
+    Remove-Item -Force -Recurse -Path "C:\Users\antip\AppData\Local\Temp\scoped_dir*" -ErrorAction SilentlyContinue
 }
