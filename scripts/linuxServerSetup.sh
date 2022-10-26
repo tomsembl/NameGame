53eabd4e27842376b4a71bfbcef1ff89c21f5a8f
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-26 21:44:00 +0800

    Update linuxServerSetup.sh

--- scripts/linuxServerSetup.sh
+++ scripts/linuxServerSetup.sh
@@ -1,15 +1,18 @@
 timedatectl set-timezone Australia/West
-
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt upgrade -y
+#sudo apt install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.11
+sudo apt-get install python3.11-dev
 sudo apt install python3-pip
-sudo apt-get install python3-dev
-sudo apt install libffi-dev
+#sudo apt install libffi-dev
 
-python3.11 -m pip install Flask
+python3.11 -m pip install Flask --upgrade pip
 python3.11 -m pip install flask-socketio
 python3.11 -m pip install gevent-websocket
+python3.11 -m pip install eventlet
+
 
 mkdir src
 cd src
