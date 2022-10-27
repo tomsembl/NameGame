9b858e7ca03cedc8b9c0320a443fff13579500da
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-27 16:42:48 +0800

    Update c.sh

--- scripts/c.sh
+++ scripts/c.sh
@@ -5,5 +5,5 @@
 # tmux send-keys -t namegame '/home/ubuntu/src/b.py' C-m
 #sudo cp /home/ubuntu/src/namegame/scripts/b.py /home/ubuntu/src/b.py
 #sudo cp /home/ubuntu/src/namegame/scripts/a.py /home/ubuntu/src/a.py
-#sudo bash /home/ubuntu/src/b.py
-mkdir /home/ubuntu/src/itWorked
+sudo bash /home/ubuntu/src/b.py
+mkdir /home/ubuntu/src/itEnded
