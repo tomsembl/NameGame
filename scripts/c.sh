20ccdab69dde56e58dfd2dfba924e5eefb6de46b
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-27 16:30:10 +0800

    Update c.sh

--- scripts/c.sh
+++ scripts/c.sh
@@ -1,8 +1,9 @@
-tmux new-session -A -s namegame #-d
+#tmux new-session -A -s namegame #-d
 # tmux send-keys -t namegame 'cd /home/ubuntu/src/namegame' C-m
 # tmux send-keys -t namegame 'cp /home/ubuntu/src/namegame/scripts/b.py /home/ubuntu/src/b.py' C-m
 # tmux send-keys -t namegame 'cp /home/ubuntu/src/namegame/scripts/a.py /home/ubuntu/src/a.py' C-m
 # tmux send-keys -t namegame '/home/ubuntu/src/b.py' C-m
-sudo cp /home/ubuntu/src/namegame/scripts/b.py /home/ubuntu/src/b.py
-sudo cp /home/ubuntu/src/namegame/scripts/a.py /home/ubuntu/src/a.py
-sudo bash /home/ubuntu/src/b.py
+#sudo cp /home/ubuntu/src/namegame/scripts/b.py /home/ubuntu/src/b.py
+#sudo cp /home/ubuntu/src/namegame/scripts/a.py /home/ubuntu/src/a.py
+#sudo bash /home/ubuntu/src/b.py
+mkdir /home/ubuntu/src/itWorked
