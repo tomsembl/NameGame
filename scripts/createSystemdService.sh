312362e3fd6b59e420f6648920bec0736b41cf78
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-27 16:57:05 +0800

    Update createSystemdService.sh

--- scripts/createSystemdService.sh
+++ scripts/createSystemdService.sh
@@ -3,9 +3,9 @@
 sudo cp ~/src/namegame/scripts/namegame.service /etc/systemd/system/namegame.service
 
 #enable the service
-sudo systemctl enable namegame.service
-sudo systemctl start namegame.service
-sudo systemctl status namegame.service
+sudo /bin/systemctl enable namegame.service
+sudo /bin/systemctl start namegame.service
+sudo /bin/systemctl status namegame.service
 
 #to stop the service
 #sudo systemctl stop namegame.service
