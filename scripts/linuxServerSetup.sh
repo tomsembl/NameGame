7fe9af6b9c4a358db906e01fa7cca367676f06a9
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-27 18:47:35 +0800

    Update linuxServerSetup.sh

--- scripts/linuxServerSetup.sh
+++ scripts/linuxServerSetup.sh
@@ -1,21 +1,36 @@
 timedatectl set-timezone Australia/West
-mkdir src
-cd src
-git clone https://github.com/tomsembl/namegame
 
 sudo apt update
 sudo apt upgrade -y
-#sudo apt install software-properties-common
-#sudo add-apt-repository ppa:deadsnakes/ppa
-#sudo apt install libffi-dev
-sudo apt install python3.11
-sudo apt-get install python3.11-dev
-sudo apt install python3-pip
 
-python3.11 -m pip install Flask --upgrade pip
-python3.11 -m pip install flask-socketio
-python3.11 -m pip install gevent-websocket
-python3.11 -m pip install eventlet
+cd /usr/local/bin
+sudo git clone https://github.com/tomsembl/namegame
+cd namegame/scripts/
+sudo chmod +x ./a.sh ./b.sh
+sudo cp ./a.sh ../../a.sh
+sudo cp ./b.sh ../../b.sh
+cd ../..
+sudo apt install python3.11
+sudo python3.11 -m pip install Flask --upgrade pip
+sudo python3.11 -m pip install flask-socketio
+sudo python3.11 -m pip install gevent-websocket
+sudo python3.11 -m pip install eventlet
+sudo ./b.sh
+
+#OLD
+# sudo apt update
+# sudo apt upgrade -y
+# # sudo apt install software-properties-common
+# # sudo add-apt-repository ppa:deadsnakes/ppa
+# # sudo apt install libffi-dev
+# sudo apt install python3.11
+# sudo apt-get install python3.11-dev
+# sudo apt install python3-pip
+
+# python3.11 -m pip install Flask --upgrade pip
+# python3.11 -m pip install flask-socketio
+# python3.11 -m pip install gevent-websocket
+# python3.11 -m pip install eventlet
 
 #optionals
 
