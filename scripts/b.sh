0a604f5e9197aeab24e050a7743802f86fd8e936
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-29 20:34:50 +0800

    Update b.sh

--- scripts/b.sh
+++ scripts/b.sh
@@ -1,3 +1,7 @@
 cd namegame
 git pull --ff
+espeak "0 0 0 Starting the NameGame and turning off the TV" 2>/dev/null
+echo "standby 0" | cec-client RPI -s -d 1
+echo 0 | sudo tee /sys/class/leds/led0/brightness
+echo 0 | sudo tee /sys/class/leds/led1/brightness
 python3.11 app.py
