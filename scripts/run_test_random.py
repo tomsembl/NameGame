fe8deb1ab500a59311acaff6b830cb2221027dd2
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-10-30 11:46:20 +0800

    Create run_test_random.py

---
+++ scripts/run_test_random.py
@@ -0,0 +1,7 @@
+﻿import os
+while True:
+    print("\n\nnum_players: random")
+    a = os.system(r"C:\github_code\NameGame\scripts\web_scrape_test.py 0 --headless")
+    print(a)
+    with open(r"C:\github_code\NameGame\scripts\test_results.txt", "a") as file_object:
+        file_object.write(str(a))
