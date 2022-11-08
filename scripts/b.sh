fb10d593ad9a356cb4d7d11dfa9b44399ccad9f8
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-11-08 22:53:28 +0800

    Update b.sh

--- scripts/b.sh
+++ scripts/b.sh
@@ -11,6 +11,7 @@ cd namegame
 git pull --ff
 espeak "0 0 0 Starting the NameGame and turning off the TV" 2>/dev/null
 echo "standby 0" | cec-client RPI -s -d 1
+/home/ubuntu/src/lan951x-led-ctl/lan951x-led-ctl --fdx=0 --lnk=0 --spd=0
 echo 0 | sudo tee /sys/class/leds/led0/brightness
 echo 0 | sudo tee /sys/class/leds/led1/brightness
 python3.11 app.py
