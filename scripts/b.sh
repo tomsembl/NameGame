32bde0602577f4817b4d99b3c14204408627dc1e
tomsembl <94670548+tomsembl@users.noreply.github.com>
2024-03-19 22:50:05 +0800

    a

--- scripts/b.sh
+++ scripts/b.sh
@@ -7,8 +7,12 @@
 # tmux send-keys -t namegame 'echo 0 | sudo tee /sys/class/leds/led1/brightness' C-m
 # tmux send-keys -t namegame 'python3.11 app.py' C-m
 
+#this starts on boot
+
 cd namegame
+git stash
 git pull --ff
+git stash pop
 espeak "0 0 0 Starting the NameGame and turning off the TV" 2>/dev/null
 echo "standby 0" | cec-client RPI -s -d 1
 /home/ubuntu/src/lan951x-led-ctl/lan951x-led-ctl --fdx=0 --lnk=0 --spd=0
